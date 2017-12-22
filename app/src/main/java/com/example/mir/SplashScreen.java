a96ccc4156de112c1d097ab7afdb1ccf6c7c13bd
ranucala <32184886+ranucala@users.noreply.github.com>
2017-12-23 04:02:50 +0700

    commit 1

---
+++ app/src/main/java/com/example/mir/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.mir;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import com.example.mir.inkom.R;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+// METHOD 1
+
+        /****** Create Thread that will sleep for 5 seconds *************/
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(3*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(getBaseContext(), BeritaUtama.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+    }
+}
