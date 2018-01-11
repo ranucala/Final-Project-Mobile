2d46422eeb8efc7a3e928be0e3eaed8643681c53
iwandar28 <muhammadichwandar@gmail.com>
2018-01-11 20:04:12 +0800

    FIX

--- app/src/main/java/com/example/mir/BeritaUtama.java
+++ app/src/main/java/com/example/mir/BeritaUtama.java
@@ -4,12 +4,14 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.mir.inkom.R;
 
@@ -33,10 +35,17 @@ public class BeritaUtama extends  AppCompatActivity{
     JSONArray string_json = null;
     ListView list;
     LazyAdapter adapter;
+    private SwipeRefreshLayout SwipeRefresh;
+    private TextView objekKata;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.beritautama);
+
+
+
 
         DaftarBerita = new ArrayList<HashMap<String, String>>();
         new AmbilData().execute();
@@ -51,13 +60,46 @@ public class BeritaUtama extends  AppCompatActivity{
                 in.putExtra(TAG_GAMBAR, map.get(TAG_GAMBAR));
                 startActivity(in);
             }
+
+
+
         });
+
+               /*menambah warna pada SwipeRefreshLayout
+        final SwipeRefreshLayout dorefresh = (SwipeRefreshLayout)findViewById(R.id.swipelayout);
+        dorefresh.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        /*event ketika widget dijalankan
+        dorefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+            public void onRefresh() {
+                refreshItem();
+            }
+
+            void refreshItem() {
+
+                onItemLoad();
+            }
+
+            void onItemLoad() {
+                dorefresh.setRefreshing(false);
+            }
+
+        });*/
+
+
     }
+
     public void SetListViewAdapter(ArrayList<HashMap<String,
             String>> berita) {
-        adapter = new LazyAdapter(this, berita);
+        adapter = new LazyAdapter(this ,berita);
         list.setAdapter(adapter);
+
     }
+
     class AmbilData extends AsyncTask<String, String, String> {
         @Override
         protected void onPreExecute() {
@@ -103,5 +145,4 @@ public class BeritaUtama extends  AppCompatActivity{
             });
         }
     }
-
 }
